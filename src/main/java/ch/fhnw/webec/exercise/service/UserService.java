0cdec14768fc0dcd96ec3a11e298d0933ff2598d
Rainer Lehner <rainer.lehner@rapp.ch>
2022-01-02 21:35:11 +0100

    preparations for authentication and authorisation added

---
+++ src/main/java/ch/fhnw/webec/exercise/service/UserService.java
@@ -0,0 +1,36 @@
+package ch.fhnw.webec.exercise.service;
+
+import ch.fhnw.webec.exercise.model.User;
+import ch.fhnw.webec.exercise.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void addUser(String username, String password, Set<String> authorities) {
+        this.userRepository.save(new User(username, this.passwordEncoder.encode(password), authorities));
+    }
+
+    public boolean usernameAlreadyExists(String username) {
+        return this.userRepository.findByUsername(username).isPresent();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found."));
+    }
+}
+
