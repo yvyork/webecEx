c726da0614205c06fa91cb31ab949532a4b14108
yves.baechtiger <yves.baechtiger@students.fhnw.ch>
2022-01-03 17:30:31 +0100

    Implement Authentication and Authorization

--- src/main/java/ch/fhnw/webec/exercise/config/AddAdminUser.java
+++ src/main/java/ch/fhnw/webec/exercise/config/AddAdminUser.java
@@ -1,35 +1,35 @@
-package ch.fhnw.webec.exercise.config;
-
-import ch.fhnw.webec.exercise.service.UserService;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
-
-import javax.validation.Valid;
-import java.util.Set;
-
-@Lazy
-@Component
-public class AddAdminUser {
-    @Value("${Spring.security.user.name}")
-    private String username;
-    @Value("${Spring.security.user.password}")
-    private String password;
-    private final UserService userService;
-
-    public AddAdminUser(UserService userService) {
-        this.userService = userService;
-    }
-
-    @EventListener(ContextRefreshedEvent.class)
-    public void onContextRefreshed() {
-        try {
-            this.userService.loadUserByUsername(this.username);
-        } catch (UsernameNotFoundException e) {
-            this.userService.addUser(this.username, this.password, Set.of("ROLE_ADMIN"));
-        }
-    }
-}
+//package ch.fhnw.webec.exercise.config;
+//
+//import ch.fhnw.webec.exercise.service.UserService;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Lazy;
+//import org.springframework.context.event.ContextRefreshedEvent;
+//import org.springframework.context.event.EventListener;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Component;
+//
+//import javax.validation.Valid;
+//import java.util.Set;
+//
+//@Lazy
+//@Component
+//public class AddAdminUser {
+//    @Value("${Spring.security.user.name}")
+//    private String username;
+//    @Value("${Spring.security.user.password}")
+//    private String password;
+//    private final UserService userService;
+//
+//    public AddAdminUser(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @EventListener(ContextRefreshedEvent.class)
+//    public void onContextRefreshed() {
+//        try {
+//            this.userService.loadUserByUsername(this.username);
+//        } catch (UsernameNotFoundException e) {
+//            this.userService.addUser(this.username, this.password, Set.of("ROLE_ADMIN"));
+//        }
+//    }
+//}
