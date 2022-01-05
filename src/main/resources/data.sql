5bdd8a90f203eeae2550791f2ab63a2d0550a126
yves.baechtiger <yves.baechtiger@students.fhnw.ch>
2022-01-05 19:53:33 +0100

    updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,14 +15,14 @@ INSERT IGNORE INTO Status (name)
 VALUES('broken');
 
 INSERT IGNORE INTO Device (serial_Number, display_size, manufacturer, memory, model, processor, purchase_date, location_id, status_id)
-VALUES ('ABC', '13"', 'Apple', '16GB', 'MacBook Air', 'Intel Chip', '2008-01-01', 1,1);
+VALUES ('C02FX13MQ6W9', 'No Information', 'Apple', '8GB', 'Mac Mini', 'M1','2020-05-08', 1,1);
 INSERT IGNORE INTO Device (serial_Number, display_size, manufacturer, memory, model, processor, purchase_date, location_id, status_id)
-VALUES ('DEF', '15"', 'Apple', '128GB', 'MacBook SuperMegaGeil', 'M5 Chip', '2008-01-01', 1,2);
+VALUES ('C07F1CXPQ6NV', '13"', 'Apple', '16GB', 'MacBook Air', 'M1','2020-11-10', 1,2);
 INSERT IGNORE INTO Device (serial_Number, display_size, manufacturer, memory, model, processor, purchase_date, location_id, status_id)
-VALUES ('GHI', '13"', 'Apple', '8GB', 'MacBook Air', 'M1 Chip', '2008-01-01', 2,3);
+VALUES ('C07CL0GFPJH7', '13"', 'Apple', '16GB', 'MacBook Pro', 'Intel', '2019-06-08', 2,3);
 INSERT IGNORE INTO Device (serial_Number, display_size, manufacturer, memory, model, processor, purchase_date, location_id, status_id)
-VALUES ('JKL', '13"', 'Apple', '16GB', 'MacBook Air', 'Intel Chip', '2008-01-01', 2,1);
+VALUES ('C02TN07YJ1GH', '13"', 'Apple', '8GB', 'iPad Air Gen.4', 'M1', '2021-05-06', 2,1);
 INSERT IGNORE INTO Device (serial_Number, display_size, manufacturer, memory, model, processor, purchase_date, location_id, status_id)
-VALUES ('MNO', '13"', 'Apple', '8GB', 'iPad Pro Gen.4 2021', 'M1','2008-01-01', 3,3);
+VALUES ('XWWTP6C4F4', '13"', 'Apple', '8GB', 'iPad Pro Gen.4', 'M1','2021-01-01', 3,3);
 
 
